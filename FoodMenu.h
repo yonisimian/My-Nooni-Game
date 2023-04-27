4e8d12e1cd8ce3d69dd61ebf03ad5382142e8a9b
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-04-27 13:13:23 +0300

    Update FoodMenu.h

--- FoodMenu.h
+++ FoodMenu.h
@@ -2,18 +2,31 @@
 
 #include "Menu.h"
 
+enum FoodType {
+	MILK = 2,
+	STRAWBERRY,
+	SALAD,
+	BURGER,
+	PIZZA,
+	CHOCOLATE,
+};
+
 //A child class of Menu - for food menu
 class FoodMenu : public Menu
 {
 public:
 	//Constructor
-	FoodMenu(gameDataRef data, SoundManage* sounds, int elementsNumber);
+	FoodMenu(gameDataRef data, int elementsNumber);
 	//Destructor
-	~FoodMenu() = default;
+	~FoodMenu() { }
+	//Handles input
+	int handleInput();
 	//Gets the xp of chosen food from menu
 	int getFoodXp(int index);
 
-private:
-	//Sets the menu's buttons
-	void setButtons();
+	const static int MAX_FOOD_ELEMENTS = 6;
+
+protected:
+	//Pushes all elements sprites into sprites vector
+	void pushSpriteElements(int elementsNumber);
 };
