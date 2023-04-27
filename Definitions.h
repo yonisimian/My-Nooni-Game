aa7e1bd6fcce27f160cb6ce2cab509982a5bd883
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-04-27 12:57:54 +0300

    Update Definitions.h

--- Definitions.h
+++ Definitions.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 //Screen size
 #define SCREEN_WIDTH 864
@@ -63,14 +63,13 @@
 
 //Accessorises information
 #define ACCESSORIES_NUBER 4
-#define NO_ACCESSORY -1
 //Accessorises files
-#define REFRIGERATOR "graphics/Accessories/refrigerator.png"
-#define BED "graphics/Accessories/bed.png"
-#define BATH "graphics/Accessories/bath.png"
-#define TABLE "graphics/Accessories/table.png"
-#define OPENED_REFRIGERATOR "graphics/Accessories/openFridge.png"
-#define USED_BED "graphics/Accessories/bedSleep.png"
+#define REFRIGERATOR_FILE "graphics/Accessories/refrigerator.png"
+#define BED_FILE "graphics/Accessories/bed.png"
+#define BATH_FILE "graphics/Accessories/bath.png"
+#define TABLE_FILE "graphics/Accessories/table.png"
+#define OPENED_REFRIGERATOR_FILE "graphics/Accessories/openFridge.png"
+#define USED_BED_FILE "graphics/Accessories/bedSleep.png"
 
 //Pam files
 #define HAPPY_PAM_WINDOW "graphics/Pam/happyPam.png"
@@ -78,13 +77,11 @@
 #define SMALL_PAM_WINDOW "graphics/Pam/pamSmall.png"
 
 //Pet folders
-#define COCO "graphics/Pet/Fox/"
-#define FIFI "graphics/Pet/Penguin/"
-#define BOO "graphics/Pet/Pug/"
-#define ANGEL "graphics/Pet/Bunny/"
+#define COCO_FOLDER "graphics/Pet/Fox/"
+#define FIFI_FOLDER "graphics/Pet/Penguin/"
+#define BOO_FOLDER "graphics/Pet/Pug/"
+#define ANGEL_FOLDER "graphics/Pet/Bunny/"
 
-//Effect information
-#define NO_EFFECT -1
 //Effect folders
 #define BATH_ANIMATION "graphics/Effects/Water/bath"
 #define SLEEP_ANIMATION "graphics/Effects/Sleep/"
@@ -113,8 +110,6 @@
 #define CHOCOLATE_ANIMATION "graphics/Food/chocolate"
 #define SALAD_ANIMATION "graphics/Food/salad"
 
-//Menu information
-#define EXIT_MENU 6
 //Menu folders
 #define FOOD_MENU "graphics/Buttons/Food/"
 #define TOY_MENU "graphics/Buttons/Toys/"
@@ -138,143 +133,25 @@
 #define LOOSE_PIECE_FILEPATH "graphics/Toys/rightPost.png"
 #define GRID_SPRITE_FILEPATH "graphics/Toys/block.png"
 
-//Pam's speech arrays
-const std::string SELECT_PAM_ARRAY [5] = {
-	"Hello!\n\nMy name is Pam and I \nLive right next door.\nI'm going on a \nVacation and I can \nTake only 3 of my \nPets with me...",
-	"\nI need your help\nWith taking care\nFor one of them\nCan you do that?..." ,
-	"\nThank you for helping\nYou are the best!\nI have to be honest\nAnd say they are\nNot usual animals...",
-	"They are creatures\nCalled Noonis.\nYou can feed, bath \nThem and play with \nThem and you can\nAlso teach them \nHow to speak! ...",
-	"For now it's only in\nIt's egg stage.\nIt will grow slowly!\nI'm sure you two will\nHave a good time \nTogether!\nPlease go next\nAnd select your Nooni..."
-};
-const std::string EGG_PAM_ARRAY[6] = {
-	"Welcome to your\nNooni's room!",
-	"You can see your Nooni\nEgg on the carpet...",
-	"The heart above him is\nA reflection of your \nNooni's needs...",
-	"On the left side of the\nScreen you can see\nYour Nooni's needs...",
-	"For now his only need\nIs being petted...",
-	"Just press on your\nNooni as long as you \nWant to pet him!..."
-};
-const std::string BABY_PAM_ARRAY[9] = { 
-		"Good news!\nYour Nooni just grew \nInto a baby...",
-		"On the left side of the\nScreen you can see\nHis new needs...",
-		"His needs are :\nBeing petted, eat, sleep, \nTake a bath and play...",
-		"Now you can press on \nThe bed to let him \nSleep...",
-		"You can press on the \nBath to let him take \nA bath...",
-		"You can press on the\nRefrigerator to feed \nHim...",
-		"You can press on the\nToy Box to play with\nHim...",
-		"You can teach him how \nTo speak by pressing\nSpeech button...",
-		"Have fun you two!\nJust don't forget taking\nCare for his needs..." 
-};
-const std::string ADULT_PAM_ARRAY[4] = {
-		"Good news!\nYour Nooni just grew \nInto an adult...",
-		"Now the refrigerator is full\nWith new different kind of\nFood..",
-		"Now you can teach him\nMore new words at the \nSpeaking class...",
-		"I'm going to come back as \nSoom as I can, please keep\nTaking care for him..."
-};
-const std::string SPEAK_PAM_ARRAY[6] = {
-		"Welcome to the \nSpeaking class...",
-		"This is the place where\nYour nooni can learn new\nWords and speak...",
-		"The words are blocked.\nIn order to open them\nAnd teach your nooni...",
-		"Please press on\nThe locked buttons...",
-		"Please give your nooni as\nMuch time as he needs\nTo complete his learning...",
-		"After finishing his learning\nYou can press on the word \nButton and he will say it..."
-};
-const std::string LOOSE_PAM_ARRAY[1] = {
-		"Hello again, neighbor!\nI heard my Nooni is very \nSick so I hurried back\nSo I can take care for him.\nWe can try next time..."
-};
-const std::string WIN_PAM_ARRAY[1] = {
-		"Hello again, neighbor!\nThank you for the good \nTreatment you gave \nMy Nooni, now I'm going back \nHome, you can take care \nOf him again next time!..."
-};
-const std::string WORDS[12] = {
-	"Nooni", "Hello", "Mommy", "Daddy", "Happy", "Sad",
-	"I ♥ U", "I can talk", "My room", "You are pretty",
-	"Let's play", "I love pizza"
+enum ActioType {
+	STAND = -1,
+	PETTED,
+	EAT,
+	SLEEP,
+	SHOWER,
+	PLAY,
+	GROW,
+	TALK,
+	INTRO,
+	OPEN_REFRIGERATOR,
+	OPEN_BOX,
+	OTHER_STATE
 };
 
-enum NooniName {
-	Angel,
-	Coco,
-	Fifi,
-	Boo
-};
-
-enum moods {
-	greenMood = 10,
-	yellowMood = 7,
-	redMood = 3,
-	blackMood = 0
-};
-
-enum age {
-	egg,
-	baby,
-	adult
-};
-
-enum food {
-	Milk,
-	Strawberry,
-	Salad,
-	Burger,
-	Pizza,
-	Chocolate,
-};
-
-enum actions {
-	beingPet, eat, sleep, takingBath, play, growing, talk, intro, stand, openRefrigerator, openBox, GoOtherState
-};
-
-enum accessories {
-	refrigerator, bed, bath, table, toyBox
-};
-
-enum pamTalking {
-	introduce,
-	eggExplanation,
-	babyExplanation,
-	adultExplanation,
-	speakExplanation,
-	loose,
-	win
-};
-
-enum effects {
-	sleepEffect,
-	bathEffect,
-	milkEffect,
-	strawberryEffect,
-	saladEffect,
-	burgerEffect,
-	pizzaEffect,
-	chocolateEffect,
-	ballEffect,
-	carEffect,
-	beingPettedEffect,
-	growingEggEffect,
-	growingBabyEffect
-};
-
-enum games {
-	ball,
-	car,
-	tic_tac_toe
-};
-
-enum backgroundSoundsType {
-	mainSound,
-	LearningSound
-};
-
-enum actionSoundsType {
-	pettingSound,
-	showerSound,
-	eatingSound,
-	growingSound,
-	sleepingSound,
-	playingSound
-};
-
-enum gameSoundsType {
-	mouseClickSound,
-	happyPetSound
+enum MoodType {
+	DEAD_MOOD = -1,
+	BLACK_MOOD,
+	RED_MOOD = 3,
+	YELLOW_MOOD = 7,
+	GREEAN_MOOD = 10
 };
