c53cbe128fa3c301c5d0366e1c70bc5d2bd91baf
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-04-27 14:22:27 +0300

    Update ToysMenu.h

--- ToysMenu.h
+++ ToysMenu.h
@@ -2,16 +2,26 @@
 
 #include "Menu.h"
 
+enum ToysType {
+	BALL = 8,
+	CAR,
+	TIC_TAC_TOE
+};
+
 //A child class of Menu - for toys menu
 class ToysMenu : public Menu
 {
 public:
 	//Constructor
-	ToysMenu(gameDataRef data, SoundManage* sounds, int elementsNumber);
+	ToysMenu(gameDataRef data, int elementsNumber);
 	//Destructor
-	~ToysMenu() = default;
+	~ToysMenu() { }
+	//Handles input
+	int handleInput();
 
-private:
-	//Sets the menu's buttons
-	void setButtons();
+	const static int MAX_TOYS_ELEMENTS = 3;
+
+protected:
+	//Pushes all elements sprites into sprites vector
+	void pushSpriteElements(int elementsNumber);
 };
